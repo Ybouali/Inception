27071b759f09074ae7be8ab92d9add96b43e02d1
Ybouali <89746106+Ybouali@users.noreply.github.com>
2022-09-10 04:35:58 +0100

    ERROR ON MARIADB SCRIPT

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+chmod 777 /var/run/mysqld/mysqld.sock
+
+/etc/init.d/mysql start
+
+mysql -u root -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+
+mysql -u root -e "GRANT ALL ON wordpress.* TO '{$MYSQL_USER}'@'%' IDENTIFIED BY '{$MYSQL_ROOT_PASSWORD}'"
+
+mysql -u root -e "FLUSH PRIVILEGES"
+
+/etc/init.d/mysql stop
+
+mysqld_safe --skip-grant-tables &
