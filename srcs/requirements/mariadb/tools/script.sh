118a58107591932ec0fe0363b64dbd2499e572ec
Ybouali <89746106+Ybouali@users.noreply.github.com>
2022-10-13 22:56:42 +0100

    ERROR ON CONNECTION WORDPRESS TO MARIADB

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-# mkdir /var/run/mysqld
-
-# touch /var/run/mysqld/mysqld.sock
-
-# # chown -R mysql:mysql /var/run/mysqld
-
-# /etc/init.d/mysql stop
-
-# /etc/init.d/mysql start
-
 service mysql start
 
 if [ -z "$(mysql -u root -e "SHOW DATABASES LIKE '${DATABASE_NAME}'" | grep ${DATABASE_NAME})" ]; then
 
-    mysql -u root -e "CREATE DATABASE ${DATABASE_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;"
     
-    # mysql -u root ${DATABASE_NAME} < /tmp/wordpress.sql
+    echo "Creating ${DATABASE_NAME} database ..."
+    # mysql -u root -e "CREATE DATABASE IF NOT EXISTS wp_db; CREATE USER IF NOT EXISTS 'wp-user'@'%' IDENTIFIED BY 'wp-pass'; GRANT ALL PRIVILEGES ON *.* TO 'wp-user'@'%'; FLUSH PRIVILEGES;"
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS wp_db;"
+    mysql -u root -e "CREATE USER IF NOT EXISTS 'wp-user'@'%' IDENTIFIED BY 'wp-pass';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'wp-user'@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
 
-    mysql -u root -e " GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
 fi
-
-# mysqladmin -u root -p $MYSQL_ROOT_PASSWORD shutdown
-
+    
 sleep 1
 
 service mysql stop
-# /etc/init.d/mysql stop
 
 mysqld
