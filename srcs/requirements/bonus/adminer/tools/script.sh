ea56725341043099ad6d68e429c6bcc022f3543f
Ybouali <89746106+Ybouali@users.noreply.github.com>
2022-10-31 02:05:01 +0100

    ADMINER DONE

--- srcs/requirements/bonus/adminer/tools/script.sh
+++ srcs/requirements/bonus/adminer/tools/script.sh
@@ -7,9 +7,6 @@ echo "Alias /adminer.php /usr/share/adminer/adminer.php" | sudo tee /etc/apache2
 a2enconf adminer.conf
 
 service apache2 restart
-# a2enconf adminer
-
-# service apache2 reload
 
 apachectl stop
 
